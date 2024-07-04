0bc235587400d1f9eb9c6cf0c99a189c01577542
pacfrog <justin.sw.yang@gmail.com>
2024-07-04 11:25:38 +0800

    test

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -14,13 +14,13 @@ fi
 bindkey -v '^?' backward-delete-char
 
 # Change cursor shape for different vi modes.
-function zle-keymap-select () {
-    case $KEYMAP in
-        vicmd) echo -ne '\e[1 q';;      # block
-        viins|main) echo -ne '\e[5 q';; # beam
-    esac
-}
-zle -N zle-keymap-select
+#function zle-keymap-select () {
+    #case $KEYMAP in
+        #vicmd) echo -ne '\e[1 q';;      # block
+        #viins|main) echo -ne '\e[5 q';; # beam
+    #esac
+#}
+#zle -N zle-keymap-select
 zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
     echo -ne "\e[5 q"
